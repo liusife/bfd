abbe6993443a784d10614e238a75ad6e276e9236
lhassis <assis.luish@gmail.com>
2025-08-19 10:38:12 -0300

    rename

--- desafio3.py
+++ desafio3.py
@@ -3,6 +3,6 @@ numero_usuario = input("Digite o seu número de Whatsapp: ")
 peso_usuario = float(input("Digite o seu peso (Kg): "))
 altura_usuario = float(input("Digite a sua altura em metros: "))
 
-imc =round(peso_usuario / (altura_usuario * altura_usuario), 2)
+imc = round(peso_usuario / (altura_usuario * altura_usuario), 2)
 consumo_agua = ((35 * peso_usuario) / 1000)
 print(f" Nome: {nome_usuario}\n Whatsapp: {numero_usuario}\n IMC: {imc}\n Consumo diário de água: {consumo_agua} litros")
