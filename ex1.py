5ef059418c6d2472b6dff356904395f3e45d8ced
lhassis <assis.luish@gmail.com>
2025-08-19 00:23:20 -0300

    first commit

---
+++ ex1.py
@@ -0,0 +1,11 @@
+valor_hora_trabalhada = int(input("Informe o valor recebido por hora trabalhada: "))
+quant_hora_mes = int(input("Infome a quantidade de horas trabalhadas no mês: "))
+
+salario_bruto = (valor_hora_trabalhada * quant_hora_mes)
+desc_ir = (salario_bruto * (11/100))
+desc_inss = (salario_bruto * (8/100))
+desc_sind = (salario_bruto * (5/100))
+
+salario_liq = (salario_bruto - (desc_ir) - (desc_inss) - (desc_sind))
+
+print(f"+ Salário Bruto: R${salario_bruto} \n- IR (11%): R${desc_ir} \n- INSS (8%): R${desc_inss} \n- Sindicato(5%): R${desc_sind} \n= Salário Líquido: R${salario_liq}")
