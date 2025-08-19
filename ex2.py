822920234277c5639e0a4ded99b984a5a813347d
lhassis <assis.luish@gmail.com>
2025-08-19 01:03:38 -0300

    2 exercicio

---
+++ ex2.py
@@ -0,0 +1,8 @@
+nome_usuario = input("Digite seu nome: ")
+numero_usuario = input("Digite o seu número de Whatsapp: ")
+peso_usuario = float(input("Digite o seu peso (Kg): "))
+altura_usuario = float(input("Digite a sua altura em metros: "))
+
+imc =round(peso_usuario / (altura_usuario * altura_usuario), 2)
+consumo_agua = ((35 * peso_usuario) / 1000)
+print(f" Nome: {nome_usuario}\n Whatsapp: {numero_usuario}\n IMC: {imc}\n Consumo diário de água: {consumo_agua} litros")
