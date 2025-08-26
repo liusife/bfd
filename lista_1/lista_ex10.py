8f83a5c730638cb255dddf5ae0c0125570bf52ff
liusife <assis.luish@gmail.com>
2025-08-26 11:41:39 -0300

    função max adicionada

--- lista_1/lista_ex10.py
+++ lista_1/lista_ex10.py
@@ -4,9 +4,11 @@ num1 = int(input("Digite o primeiro número: "))
 num2 = int(input("Digite o segundo número: ")) 
 num3 = int(input("Digite o terceiro número: "))
 
-if num1 >= num2 and num1 >= num3:
-    print(f"O maior número é: {num1}")
-elif num2 >= num1 and num2 >= num3:
-    print(f"O maior número é: {num2}")
-else:
-    print(f"O maior número é: {num3}")
+# if num1 >= num2 and num1 >= num3:
+#     print(f"O maior número é: {num1}")
+# elif num2 >= num1 and num2 >= num3:
+#     print(f"O maior número é: {num2}")
+# else:
+#     print(f"O maior número é: {num3}")
+
+print(f"O maior número é: {max(num1, num2, num3)}")
