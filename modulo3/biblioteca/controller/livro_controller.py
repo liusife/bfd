80b807e8fa66a1a9dc97b157a006dc886bd4e91e
liusife <assis.luish@gmail.com>
2025-10-30 11:49:54 -0300

    Correção de erro

--- modulo3/biblioteca/controller/livro_controller.py
+++ modulo3/biblioteca/controller/livro_controller.py
@@ -1,15 +1,14 @@
 from model.livro_model import Livro
 from service.livro_service import salvar_livro, listar_livros
 
-def cadastrar_livro(isbn: str, titulo: str, usuario_cpf: str):
+def cadastrar_livro(isbn, titulo, usuario_cpf):
     if not isbn or not titulo or not usuario_cpf:
-        return "Preencha todos os campos!"
-    livro = Livro(isbn, titulo, usuario_cpf)
+        return "ISBN, título e CPF do usuário são obrigatórios!"
     try:
-        salvar_livro(livro)
-        return f"Livro '{titulo}' cadastrado com sucesso!"
+        salvar_livro(isbn, titulo, usuario_cpf)
+        return "Livro cadastrado com sucesso!"
     except Exception as e:
-        return f"Erro ao cadastrar livro: {e}"
-    
+        return f"Erro ao cadastrar livro: {str(e)}"
+
 def obter_livros():
     return listar_livros()
